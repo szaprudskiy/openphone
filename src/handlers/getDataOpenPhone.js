9cfe533d1b12a74170c7e03403bd9ca0e9b4e9c0
szaprudskiy <sergei.zaprudskiy@gmail.com>
2024-06-17 17:44:53 +0300

    reg

--- src/handlers/getDataOpenPhone.js
+++ src/handlers/getDataOpenPhone.js
@@ -10,7 +10,7 @@ const getDataOpenPhone = async (req, res) => {
     const {
       type,
       data: { object: eventData },
-    } = req.body.object
+    } = req.body
 
     // console.log('Received webhook event:', type, eventData)
 
