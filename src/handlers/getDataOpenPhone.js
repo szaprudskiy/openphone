28d11f55cba04e21ec13bbbf67452b4503585086
szaprudskiy <sergei.zaprudskiy@gmail.com>
2024-06-20 22:02:57 +0300

    greher

--- src/handlers/getDataOpenPhone.js
+++ src/handlers/getDataOpenPhone.js
@@ -27,7 +27,7 @@ const getDataOpenPhone = async (req, res) => {
     const {
       type,
       data: { object: eventData },
-    } = req.body.object
+    } = req.body
 
     const { from, to, media, body } = eventData
 
