d51963f51fd98df5967e0c5527b4985ebd437cfa
szaprudskiy <sergei.zaprudskiy@gmail.com>
2024-06-20 19:33:44 +0300

    trhr

--- src/handlers/getDataOpenPhone.js
+++ src/handlers/getDataOpenPhone.js
@@ -4,10 +4,8 @@ import updateContactWithIncomingMessage from '../services/updateContactWithIncom
 import updateContactWithOutgoingMessage from '../services/updateContactWithOutgoingMessage.js'
 import updateContactWithRecording from '../services/updateContactWithCallRecording.js'
 import createContactInZohoCRM from '../services/createContactInZohoCRM.js'
-import RequestQueue from 'node-request-queue'
 
 const excludedNumbers = ['+1 (727) 966-2707', '+1 (737) 345-3339']
-const queueMap = new Map()
 
 const getDataOpenPhone = async (req, res) => {
   try {
@@ -21,6 +19,7 @@ const getDataOpenPhone = async (req, res) => {
     const formattedFrom = formatPhoneNumber(from)
     const formattedTo = formatPhoneNumber(to)
 
+    let contact = null
     let validNumber = null
 
     if (!excludedNumbers.includes(formattedFrom)) {
@@ -29,56 +28,50 @@ const getDataOpenPhone = async (req, res) => {
       validNumber = formattedTo
     }
 
-    if (!validNumber) {
-      return res.status(404).json({ message: 'Valid number not found' })
-    }
-
-    // Ensure each validNumber has its own queue
-    if (!queueMap.has(validNumber)) {
-      queueMap.set(validNumber, new RequestQueue({ concurrency: 1 }))
-    }
-
-    const queue = queueMap.get(validNumber)
-
-    const contact = await queue.add(async () => {
-      let existingContact = await findContactInZohoCRM(validNumber)
-
-      if (!existingContact) {
-        existingContact = await createContactInZohoCRM(
+    if (validNumber) {
+      contact = await findContactInZohoCRM(validNumber)
+      if (!contact) {
+        contact = await createContactInZohoCRM(
           validNumber,
           media ? media[0]?.url : null,
           body,
           type
         )
+        if (!contact) {
+          return res
+            .status(500)
+            .json({ error: 'Error creating/updating contact in Zoho CRM' })
+        } else {
+          return res
+            .status(200)
+            .json({ message: 'Creating/updating contact in Zoho CRM', contact })
+        }
       }
+    }
 
-      return existingContact
-    })
-
-    if (!contact) {
+    if (contact) {
+      if (type === 'call.recording.completed') {
+        const result = await updateContactWithRecording(
+          contact.id,
+          media[0].url
+        )
+        return res
+          .status(200)
+          .json({ message: 'Call recording added successfully', result })
+      } else if (type === 'message.received') {
+        const result = await updateContactWithIncomingMessage(contact.id, body)
+        return res
+          .status(200)
+          .json({ message: 'Incoming Message added successfully', result })
+      } else if (type === 'message.delivered') {
+        const result = await updateContactWithOutgoingMessage(contact.id, body)
+        return res
+          .status(200)
+          .json({ message: 'Outgoing Message added successfully', result })
+      }
+    } else {
       return res.status(404).json({ message: 'Contact not found' })
     }
-
-    let result = null
-
-    if (type === 'call.recording.completed') {
-      result = await updateContactWithRecording(contact.id, media[0].url)
-      return res
-        .status(200)
-        .json({ message: 'Call recording added successfully', result })
-    } else if (type === 'message.received') {
-      result = await updateContactWithIncomingMessage(contact.id, body)
-      return res
-        .status(200)
-        .json({ message: 'Incoming Message added successfully', result })
-    } else if (type === 'message.delivered') {
-      result = await updateContactWithOutgoingMessage(contact.id, body)
-      return res
-        .status(200)
-        .json({ message: 'Outgoing Message added successfully', result })
-    }
-
-    return res.status(404).json({ message: 'Operation not supported' })
   } catch (error) {
     console.error('Error processing webhook:', error)
     return res.status(500).json({ error: 'Internal Server Error' })
