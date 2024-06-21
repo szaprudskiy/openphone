d086a1cb8b73ef062b4e70d702c641dab4e83747
szaprudskiy <sergei.zaprudskiy@gmail.com>
2024-06-21 15:37:54 +0300

    interval: 80000

--- src/handlers/getDataOpenPhone.js
+++ src/handlers/getDataOpenPhone.js
@@ -10,7 +10,7 @@ const excludedNumbers = ['+1 (727) 966-2707', '+1 (737) 345-3339']
 
 const queue = new Queue({
   concurrent: 1,
-  interval: 120000,
+  interval: 80000,
 })
 
 queue.on('resolve', (data) => {
