58f3152abea484a224d59561d3c7382044f556d0
szaprudskiy <sergei.zaprudskiy@gmail.com>
2024-06-21 17:42:32 +0300

    type

--- src/handlers/getDataOpenPhone.js
+++ src/handlers/getDataOpenPhone.js
@@ -25,14 +25,16 @@ const getDataOpenPhone = async (req, res) => {
   try {
     if (!req.body || !req.body.object) {
       console.error('Invalid request body', req.body)
-      return res.status(400).json({ error: 'Invalid request body' })
     }
 
     const { type, data } = req.body.object
 
     if (!data || !data.object) {
       console.error('Invalid data object', data)
-      return res.status(400).json({ error: 'Invalid data object' })
+    }
+
+    if (!type) {
+      console.error('type', type)
     }
 
     const eventData = data.object
